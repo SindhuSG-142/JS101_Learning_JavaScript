4a26821b109560c6a31e0315c158780e3db4b4df
unknown <sgsindhusrinivas@gmail.com>
2023-11-27 21:46:02 +0530

    programs on mathematical operators

---
+++ Day2/greater number.js
@@ -0,0 +1,14 @@
+let a = 20;
+let b = 10;
+if(a > b)
+{
+  console.log("a is greater than b");
+}
+else if(a < b)
+{
+  console.log("b is greater than a");
+}
+else
+{
+  console.log("Both equal");
+}
---
+++ Day2/license.js
@@ -0,0 +1,9 @@
+let age=16;
+if(age >= 18)
+{
+  console.log("Apply for license");
+}
+else
+{
+  console.log("NA");
+}
